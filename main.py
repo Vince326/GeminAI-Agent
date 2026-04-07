990713dc83dc7c5269cb5d1bcefae1aa07c38f78
Vincent Hunter <jhearst29@gmail.com>
2026-04-06 21:44:23 -0400

    Restructured and organized the main.py file; created functions for print_response and generate_content and moved the appropriate code there; made response code, client and message code into their own variables for better reading

--- main.py
+++ main.py
@@ -7,31 +7,35 @@ from google import genai
 from google.genai import types
 
 
+
+def generate_content(client, messages):
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    return response
+
+def print_response(response):
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage metadata is not available in the response.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    
 def main():
+    load_dotenv()
+
     parser = argparse.ArgumentParser(description="Generate content using Gemini API.")
     parser.add_argument("user_prompt", type=str, help="The prompt to generate content from.")
     args = parser.parse_args()
 
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-    load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     if api_key is None:
-        raise RuntimeError("GEMINI_API_KEY is not set in the environment variables.")
+        raise RuntimeError("GEMINI_API_KEY is not set.")
 
     client = genai.Client(api_key=api_key)
-
-    gen_content = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
-
-    if gen_content.usage_metadata is None:
-        raise RuntimeError("Usage metadata is not available in the response.")
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
-    print(f"Prompt tokens: {gen_content.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {gen_content.usage_metadata.candidates_token_count}")
-
-    print(gen_content.text)
-
+    response = generate_content(client, messages)
+    print_response(response)
+    print(response.text)
 
 if __name__ == "__main__":
     main()
